9e4a5c8ddb3685c164e900fbd7dd6a2808e2a8b7
Daniel Yo <dnl992004@yahoo.com.au>
2015-10-19 21:49:10 -0700

    edited Doctor and added Staff file

--- model/Doctor.java
+++ model/Doctor.java
@@ -1,17 +1,36 @@
 package model;
+import java.text.*;
 
 public class Doctor {
 	public String firstName;
 	public String lastName;
 	public String userName;
+    private int docId;
 	public BusinessHours availble;
 	String specialty;
 	
-	public Doctor(String first, String last, String specialize ){
+   public Doctor()// initialize first
+   {
+    firstName = new String("");
+    lastName = new String("");
+    docId = 0;
+    userName = new String("");
+   }
+
+   public void setDoctor(String first, String last, String specialize, int id)
+   {
+        firstName = first;
+		lastName = last;
+		specialty = specialize;
+        docId = id;
+   
+   }
+	/*public Doctor(String first, String last, String specialize, int id){
 		firstName = first;
 		lastName = last;
 		specialty = specialize;
-	}
+        docId = id;
+	}*/
 	
 	public void setHours(int weekday, int startHour, int endHour){
 		availble = new BusinessHours(weekday,startHour,endHour);
@@ -36,5 +55,36 @@ public class Doctor {
 	
 	//public Lab getLabRecord()
 	
-	//publid void e-Prescribe()
+	/*public void e-Prescribe(Patient p, int doctorId, String prescribe)// File prescription as 3rd param??
+    {
+        //get Patient id
+        //set Patient.prescription = prescribe;
+        //
+        return;
+    }*/
+    //public Lab assignLabTest()
+    
+    //public Lab printLabTest()
+    
+   public String getFirstName()
+   {
+    return firstName;
+   }
+
+  public String getLastName()
+   {
+    return lastName;
+   }
+
+  public int getID()
+   {
+    return docId;
+   }
+   
+   public String getUserName()
+   {
+    return userName;
+   }
+
+    
 }
---
+++ model/Staff.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.text.*;
+
+public class Staff
+{
+    private String firstName;
+    private String lastName;
+    private int ID;
+    private String identity; // lab staff, HSP staff, nurses or pharmacists
+    private String userName;
+
+    public Staff()
+    {
+        firstName = new String("");
+        lastName = new String("");
+        ID = 0;
+        userName = new String("");
+        identity = new String("");
+    }
+    
+    public void setStaff(String first, String last, int id, String user, String iden)
+    {
+        firstName = first;
+        lastName = last;
+        ID = id;
+        userName = user;
+        identity = iden;
+    }
+    
+    public Patient searchPatient(Patient[] p, String name)
+    {
+		for(int i = 0; i < p.length; i++){
+			String fullName = p[i].firstName + p[i].lastName;
+			if(name.equals(fullName)){
+				return p[i];
+			}
+		}
+		return null;
+	}
+    
+    public int getPatientInfo(Patient p)
+    {
+        int i = 0;
+        if(identity != "HSP")
+        {
+            i = 0; // 0 = false, 1= true
+        }
+        
+        return i; //if return val is 0, they cant access it
+    }
+
+}
