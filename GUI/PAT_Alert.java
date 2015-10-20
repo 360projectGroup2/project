f05bbbc7415402bd19d8f972ab1938ffead86b4b
D Martin <dlmart11@asu.edu>
2015-10-19 22:06:54 -0700

    Added a shorter GUI for alerts.

---
+++ GUI/PAT_Alert.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PAT_Alert extends JPanel {
+	public JLabel patientName, condition;
+	public JButton closeButton;
+
+	/**
+	 * Create the panel.
+	 */
+	public PAT_Alert(ActionListener b, String pName, String pCond) {
+		
+		setLayout(null);
+		setSize(300,300);
+		
+		this.patientName = new JLabel(pName);
+		this.patientName.setBounds(95, 11, 128, 14);
+		add(this.patientName);
+		
+		this.condition = new JLabel(pCond);
+		this.condition.setBounds(10, 39, 118, 14);
+		add(this.condition);
+		
+		closeButton = new JButton("Update");
+		closeButton.setBounds(245, 259, 89, 23);
+		add(closeButton );
+		closeButton.addActionListener(b);
+	}
+}
