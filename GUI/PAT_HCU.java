c4527049bf28c10a9dc7ffe129ec85b697539e39
arminpourdehghan <arminpourdehghan@gmail.com>
2015-10-19 23:12:17 -0700

    send alert updates

--- GUI/PAT_HCU.java
+++ GUI/PAT_HCU.java
@@ -1,9 +1,11 @@
 package GUI;
 
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.TextArea;
 import java.awt.Button;
 import java.awt.event.ActionListener;
@@ -20,6 +22,7 @@ public class PAT_HCU extends JPanel {
 	/**
 	 * Create the panel.
 	 */
+	@SuppressWarnings("unchecked")
 	public PAT_HCU(ActionListener b) {
 		
 		setLayout(null);
@@ -105,10 +108,13 @@ public class PAT_HCU extends JPanel {
 		textAreaAllergies.setBounds(104, 563, 380, 75);
 		add(textAreaAllergies);
 		
+		
 		updateButton = new JButton("Update");
 		updateButton.setBounds(545, 559, 89, 23);
 		add(updateButton );
 		updateButton.addActionListener(b);
+		
+		
 		
 	
 	}
--- GUI/SendAlert.java
+++ GUI/SendAlert.java
@@ -1,9 +1,11 @@
 package GUI;
 
 import java.awt.TextArea;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -12,10 +14,9 @@ public class SendAlert extends JPanel {
 	
 	public JTextField textFieldFName;
 	public JTextField textFieldLName;
-	public TextArea textAreaMedicalHistory;
-	public TextArea textAreaAllergies;
+	public TextArea textConcern;
 	public JButton updateButton;
-	
+	public JComboBox healthRankDrop;
 
 	/**
 	 * Create the panel.
@@ -46,9 +47,26 @@ public class SendAlert extends JPanel {
 	label_1.setBounds(10, 563, 100, 20);
 	add(label_1);
 	
-	textAreaAllergies = new TextArea();
-	textAreaAllergies.setBounds(104, 563, 380, 75);
-	add(textAreaAllergies);
+	textConcern = new TextArea();
+	textConcern.setBounds(104, 563, 380, 75);
+	add(textConcern);
+	
+	
+	updateButton = new JButton("Update");
+	updateButton.setBounds(545, 559, 89, 23);
+	add(updateButton );
+	updateButton.addActionListener(b);
+	
+	JLabel lblRank = new JLabel("Rank your pain:");
+	lblFirstName.setBounds(100, 559, 100, 14);
+	add(lblRank);
+	
+	
+	String [] problems = {"1","2","3","4","5","6","7","8","9","10"};
+	healthRankDrop = new JComboBox<Object>(problems);
+	healthRankDrop.setBounds(545, 559, 100, 23);
+	add(healthRankDrop);
+	healthRankDrop.addActionListener(b);
 	
 	
 	
