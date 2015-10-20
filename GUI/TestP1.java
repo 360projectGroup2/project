296b3054c60e95fe9c080c343c982d5a4847cf53
jshaller <jshaller@asu.edu>
2015-10-20 12:18:26 -0700

    Update TestP1.java

--- GUI/TestP1.java
+++ GUI/TestP1.java
@@ -11,7 +11,7 @@ public class TestP1 extends JPanel {
 	/**
 	 * This panel is for testing the first set of instructions required
 	 */
-	public JButton savebutton,ScheduleAppBtn,UpdateHealthCondBtn, sendAlertBtn;
+	public JButton savebutton,ScheduleAppBtn,UpdateHealthCondBtn, sendAlertBtn, docPanBtn;
 	
 	public TestP1(ActionListener e) {
 		setLayout(null);
@@ -22,25 +22,29 @@ public class TestP1 extends JPanel {
 		add(lblClickOnOne);
 		
 		savebutton = new JButton("Registration");
-		savebutton.setBounds(39, 61, 158, 23);
+		savebutton.setBounds(39, 36, 158, 23);
 		savebutton.addActionListener(e);
 		add(savebutton);
 		
 		ScheduleAppBtn = new JButton("Schedule Appointments");
-		ScheduleAppBtn.setBounds(39, 124, 174, 23);
+		ScheduleAppBtn.setBounds(39, 96, 174, 23);
 		ScheduleAppBtn.addActionListener(e);
 		add(ScheduleAppBtn);
 		
 		UpdateHealthCondBtn = new JButton("Update Health Conditions");
-		UpdateHealthCondBtn.setBounds(39, 196, 174, 23);
+		UpdateHealthCondBtn.setBounds(39, 156, 174, 23);
 		UpdateHealthCondBtn.addActionListener(e);
 		add(UpdateHealthCondBtn);
 		
 		sendAlertBtn = new JButton("Send Alert");
-		sendAlertBtn.setBounds(39, 270, 174, 23);
+		sendAlertBtn.setBounds(39, 216, 174, 23);
 		sendAlertBtn.addActionListener(e);
-		add(sendAlertBtn );
+		add(sendAlertBtn);
 		
+		docPanBtn = new JButton("Doctor Panel");
+		docPanBtn.setBounds(39, 276, 158, 23);
+		docPanBtn.addActionListener(e);
+		add(docPanBtn);
 
 	}
 
