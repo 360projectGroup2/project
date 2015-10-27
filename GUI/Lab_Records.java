4ef0d6672a0e7f0919f3a291240a74cb094804a4
tshana <taseen.shana@gmail.com>
2015-10-27 01:02:09 -0700

    Lab_Records GUI fixed. Now displays

--- GUI/Lab_Records.java
+++ GUI/Lab_Records.java
@@ -1,3 +1,4 @@
+// 10-27-15 1:00 AM UPDATE
 package GUI;
 
 import java.awt.*;
@@ -8,77 +9,73 @@ import javax.swing.*;
 public class Lab_Records extends JPanel implements ActionListener{
 	// INSTANCE VARIABLES	
 
-private JLabel pNameLabel, patientID, consultationID, testID,patientKey;
-private JTextField name, pID,consultID, testIdent,pKey;
-private JButton view, save; 
+public JLabel pNameLabel, patientID, consultationID, testID,patientKey;
+public JTextField name, pID,consultID, testIdent,pKey;
+public JButton view, save; 
 public String storeName, storePatientID, storeConsultID,storeTestID, storePkey;
 		
-//pushing
 
 	public Lab_Records(ActionListener b) {
 		setLayout(null);
 		setSize(500,300);
 	
-		//CREATE PANELS	
-		JPanel wholePanel = new JPanel();
-		JPanel labelPanel = new JPanel(); // Left side of whole Panel
-		JPanel textfPanel = new JPanel(); // Right side of whole Panel
-	   
 
 		// CREATE LABELS / TEXTFIELDS		
 		pNameLabel		= new JLabel("Patient Name:");
+		pNameLabel.setBounds(30, 59, 90, 14);
+		add(pNameLabel);
+		
 		patientID 	 	= new JLabel("Patient ID:");
+		patientID.setBounds(30, 90, 65, 14);
+		add(patientID);
+
 		consultationID  = new JLabel("Consultation ID:");
+		consultationID.setBounds(30, 121, 120, 14);
+		add(consultationID);
+		
 		testID   		= new JLabel("Test ID:");
+		testID.setBounds(30, 152, 65, 14);
+		add(testID);
+		
 		patientKey    	= new JLabel("Specialized Patient Key:");
+		patientKey.setBounds(30, 183, 150, 14);
+		add(patientKey);
 		save 			= new JButton("Save");
+		save.setBounds(180, 211, 86, 20);
+		add(save);
+		
 		view 			= new JButton("View");
+		view.setBounds(180, 242, 86, 20);
+		add(view);
 		save.addActionListener(this);
 		view.addActionListener(this);
 
 		//Text Field Initialization	
 		name = new JTextField();
+		name.setBounds(180, 56, 86, 20);
+		add(name);
+		name.setColumns(10);
+		
 		pID = new JTextField();
+		pID.setColumns(20);
+		pID.setBounds(180, 87, 86, 20);
+		add(pID);
+		
 		consultID = new JTextField();
+		consultID.setColumns(20);
+		consultID.setBounds(180, 118, 86, 20);
+		add(consultID);
+		
 		testIdent = new JTextField();
+		testIdent.setColumns(20);
+		testIdent.setBounds(180, 149, 86, 20);
+		add(testIdent);
+		
 		pKey = new JTextField();
-
-	// LabelPanel -- (Left Side)
-		labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
+		pKey.setColumns(20);
+		pKey.setBounds(180, 180, 86, 20);
+		add(pKey);
 		
-		labelPanel.add(Box.createVerticalStrut(10)); //Adds an invisible "box"
-		labelPanel.add(pNameLabel);
-		labelPanel.add(Box.createVerticalStrut(10));
-			
-		labelPanel.add(patientID);
-		labelPanel.add(Box.createVerticalStrut(10));
-			
-		labelPanel.add(consultationID);
-		labelPanel.add(Box.createVerticalStrut(10));
-			
-		labelPanel.add(testID);
-		labelPanel.add(Box.createVerticalStrut(10));
-		
-		labelPanel.add(patientKey);
-		labelPanel.add(Box.createVerticalStrut(10));
-	
-	// Text Panel -- (Right Side)		
-		textfPanel.setLayout(new BoxLayout(textfPanel, BoxLayout.Y_AXIS));
-		
-		textfPanel.add(name);
-		textfPanel.add(pID);
-		textfPanel.add(consultID);
-		textfPanel.add(testIdent);
-		textfPanel.add(pKey);
-		textfPanel.add(save);
-		textfPanel.add(view);
-
-		//PIECES THE LABEL AND TEXTS TOGETHER
-			wholePanel.setLayout(new GridLayout(0,3));
-			wholePanel.add(labelPanel);
-			wholePanel.add(textfPanel);
-		    add(wholePanel);
-		    
 	  } // End Controller	
 	public void actionPerformed(ActionEvent e) {
 		//Get Text
