d25414b128b4541fe1ec516ad938d9840ab21714
jshaller <jshaller@asu.edu>
2015-11-17 05:54:57 -0700

    REG_CI NEEDS SOME CODE TO CONNECT TO DATABASE

--- GUI/ApplicationGUI.java
+++ GUI/ApplicationGUI.java
@@ -216,6 +216,12 @@ public class ApplicationGUI extends JFrame {
 					updateStart(general2);
 					switch1(general2, general2.switchPanel, hspPanel);
 				}
+				else if(user.equals("Nurse"){
+					updateStart(general2);
+				}
+				else if(user.equals("Pharmacist"){
+					updateStart(general2);
+				}
 				
 			}
 			
