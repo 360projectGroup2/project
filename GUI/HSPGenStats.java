c6a6e0053be035a3c7970eed083d01d94f785714
jshaller <jshaller@asu.edu>
2015-11-17 21:33:23 -0700

    Update HSPGenStats.java

--- GUI/HSPGenStats.java
+++ GUI/HSPGenStats.java
@@ -1,19 +1,21 @@
-package GUI;
+package HSP;
 
+import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import java.awt.BorderLayout;
 
-
-
+import org.jfree.chart.ChartPanel;
 import org.jfree.ui.RefineryUtilities;
+
+import java.awt.BorderLayout;
 
 public class HSPGenStats extends JPanel implements ActionListener{
 
@@ -25,30 +27,38 @@ public class HSPGenStats extends JPanel implements ActionListener{
 	public JRadioButton[] buttons;
 	public ButtonGroup asdf;
 	public JPanel chartPanel;
+	public ChartPanel currentChart;
 	public JButton btnBack;
-
 
 	public HSPGenStats(ActionListener e, ActionListener b){
 		setLayout(null);
 		generateStats = new JLabel("Press 1 to generate next statistics");
 		generateStats.setBounds(78, 9, 165, 14);
-		this.add(generateStats); 
+		this.add(generateStats);	
 
 		genStats = new JButton("Generate Statistics");
 		genStats.setBounds(248,5,123,23);
-		genStats.addActionListener(b);
 		genStats.addActionListener(this);
+		genStats.addActionListener(b);
 		this.add(genStats);
-		
+
 		chartPanel = new JPanel();
 		chartPanel.setBounds(124, 53, 468, 410);
+		this.add(chartPanel);
 		chartPanel.setLayout(new BorderLayout(0, 0));
-		add(chartPanel);
+		//chartPanel.add(lol,BorderLayout.CENTER);
+
+		btnBack = new JButton("Back");
+		btnBack.setBounds(10, 440, 89, 23);
+		btnBack.addActionListener(e);
+		add(btnBack);
+
 
 		buttons = new JRadioButton[6];
 		asdf = new ButtonGroup();
 		for(int i=0; i<buttons.length; i++){
 			buttons[i]= new JRadioButton(statistics[i]);
+			buttons[i].addActionListener(this);
 			asdf.add(buttons[i]);
 		}
 
@@ -56,26 +66,38 @@ public class HSPGenStats extends JPanel implements ActionListener{
 		for(int i=0; i<buttons.length; i++){
 			buttons[i].setBounds(0, 50+30*i, 200, 20);
 		}
-		
-				
-		btnBack = new JButton("Back");
-		btnBack.setBounds(10, 440, 89, 23);
-		btnBack.addActionListener(e);
-		add(btnBack);
-
+		for(int i=0;i<statistics.length; i++){
+			chart.add(new BarChartDemo("Patient Statistics", statistics[i]+" Statistics", statistics[i]));
+		}
 
 
 
 	}
 	public void actionPerformed(ActionEvent e) {
-		Object source = e.getSource();
-		if(source == genStats){
+		System.out.println(e.getSource());
+		if(e.getSource() == genStats){
 			for(int i=0; i<buttons.length; i++){
 				add(buttons[i]);
 				buttons[i].setVisible(true);
+				//System.out.println("hello");
 			}
+			buttons[0].setEnabled(true);
+			currentChart=chart.get(0).chartPanel;
+			chartPanel.add(currentChart, BorderLayout.CENTER);
+			revalidate();
+			repaint();
+
 		}
-
-
+		else{
+			for(int i=0;i<buttons.length; i++)
+				if(buttons[i].isSelected()){
+					chartPanel.remove(currentChart);
+					currentChart=chart.get(i).chartPanel;
+					chartPanel.add(currentChart, BorderLayout.CENTER);
+					chartPanel.revalidate();
+					chartPanel.repaint();
+				}
+		}
 	}
+
 }
